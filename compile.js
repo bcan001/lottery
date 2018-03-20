d6f99bb35c8a81c3f58a5891f30abce53c6c90c4
Ben <ben@ben.kleinschmidt.com>
2018-03-20 14:34:57 -0500

    first push

---
+++ compile.js
@@ -0,0 +1,8 @@
+const path = require('path'); // path module
+const fs = require('fs'); // file sync module
+const solc = require('solc'); // solidity compiler module
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // bring in files
+const source = fs.readFileSync(inboxPath, 'utf8'); // read in contents of the file
+
+console.log(solc.compile(source, 1)); // log what compiler is doing
