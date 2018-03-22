f5b64e22b13cd3f8c0d767f023fe1d60a8fe01ad
Ben <ben@ben.kleinschmidt.com>
2018-03-22 16:32:55 -0500

    read

--- compile.js
+++ compile.js
@@ -5,4 +5,6 @@ const solc = require('solc'); // solidity compiler module
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol'); // bring in files
 const source = fs.readFileSync(inboxPath, 'utf8'); // read in contents of the file
 
-console.log(solc.compile(source, 1)); // log what compiler is doing
+// console.log(solc.compile(source, 1)); // log what compiler is doing
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // compile the file
+
---
+++ test/inbox.test.js
@@ -0,0 +1,89 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // capitalize because Web3 is a constructor function
+const web3 = new Web3(ganache.provider()); // lowercase because its an instance function
+
+// EXAMPLES FOR USING MOCHA JS TESTING FRAMEWORK:
+
+// class Car {
+// 	park() {
+// 		return 'stopped';
+// 	}
+
+// 	drive() {
+// 		return 'vroom';
+// 	}
+// }
+
+
+// let car;
+
+// beforeEach(() => {
+// 	car = new Car();
+// });
+
+// describe('Car', () => {
+// 	it('can park', () => {
+// 		assert.equal(car.park(), 'stopped');
+// 	});
+
+// 	it('can drive', () => {
+// 		assert.equal(car.drive(), 'vroom');
+// 	});
+// });
+
+
+
+
+
+// REAL CODE:
+
+let accounts;
+
+beforeEach(async () => {
+	// Get a list of all accounts
+	accounts = await web3.eth.getAccounts();
+
+
+
+
+
+
+	// Use one of those accounts to deploy the contract
+
+});
+
+
+
+describe('Inbox', () => {
+	it('deploys a contract', () => {
+		console.log(accounts);
+
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
